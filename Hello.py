d9fb47f448ad661d605279c5321ae31c62e99117
wangzhaohan2910 <158981244+wangzhaohan2910@users.noreply.github.com>
2024-02-05 16:48:16 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,3 +3,4 @@ print("I am wangzhaohan2910")
 print("Python is my favourite language.")
 print("I use python to make some funny stuff.")
 print("And in the future, I will put some stuff I made on github.")
+import this
