f6b8014bcd2169cb913e24769d0eaf9c2f23eda1
wangzhaohan2910 <158981244+wangzhaohan2910@users.noreply.github.com>
2024-02-05 11:18:41 +0800

    Update and rename a.cpp to Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,8 @@
+#include<bits/extc++.h>
+using namespace std;
+int main(){
+    cout<<"Hello, Github!\n";
+    cout<<"I am wangzhaohan2910\n";
+    cout<<"And, C++ is one of the languages I like to use.";
+    return 0;
+}
--- a.cpp
+++
@@ -1,6 +0,0 @@
-#include<bits/extc++.h>
-using namespace std;
-int main(){
-    cout<<"Hello, Github!";
-    return 0;
-}
