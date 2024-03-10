fd6dcf9c7affe9268b77fcdc2de9551022405450
wangzhaohan2910 <wangcf3000@sina.com>
2024-03-10 12:49:09 +0800

    This time I improved __main__.py

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 print("Hello, Github!")
-print("I am wangzhaohan2910")
+print("I am @wangzhaohan2910.")
 print("Python is my favourite language.")
 print("I use python to make some funny stuff.")
 print("And in the future, I will put some stuff I made on github.")
