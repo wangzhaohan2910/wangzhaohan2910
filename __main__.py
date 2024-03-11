37d6f0b4cf49c0b1b650b76a5756217d9d50efa6
wangzhaohan2910 <wangcf3000@sina.com>
2024-03-11 13:10:38 +0800

    Just save README.md

--- __main__.py
+++
@@ -1,6 +0,0 @@
-print("Hello, Github!")
-print("I am @wangzhaohan2910.")
-print("Python is my favourite language.")
-print("I use python to make some funny stuff.")
-print("And in the future, I will put some stuff I made on github.")
-import this
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/extc++.h>
-using namespace std;
-int main(){
-    cout<<"Hello, Github!\n";
-    cout<<"I am wangzhaohan2910\n";
-    cout<<"And, C++ is one of the languages I like to use.";
-    return 0;
-}
